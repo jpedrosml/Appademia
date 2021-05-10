ad7fabbf15d0c4655c206cd43d25011c342ad50e
ufcg04 <joao.melo@ccc.ufcg.edu.br>
2021-05-10 16:05:39 -0300

    ...

--- prolog/appademia.pl
+++ prolog/appademia.pl
@@ -1,33 +1,69 @@
 usersData([]).
 
-addUsersInfo(Info, Data, [Info|Data]).
+readString(S) :-
+read_line_to_codes(user_input, I), atom_string(I, S).
 
-input(X,Y):-
-   write(X),
-   write(' '),
-   read(Y).
+readNumber(N) :-
+   read_line_to_codes(user_input, I), number_codes(N, I)
+. 
 
+addUsersInfo(Info, Data, [Info|Data]):-!.
 
-cadastrarUsuario(Data) :-
-   input('Crie um ID numerico. Ele servira para quaisquer operacao no aplicativo: ', Id ),
-   input('Nome completo: ', Nome),
-   input('Idade: ', Idade),
-   input('Peso', Peso),
-   input('Altura', Altura),
-   input('Plano de Pagamento[mensal/trimestral/anual]: ', Plano),
-   input('Primeira vez em uma academia?[sim/nao]:', Resposta),
+signUpUser(Data) :-
+   write('Crie um ID numerico. Ele servira para quaisquer operacao no aplicativo: '), nl,
+   readNumber(Id),
+   write('Nome completo: '), nl,
+   readString(Nome),
+   write('Idade: '), nl,
+   readNumber(Idade),
+   write('Peso'), nl,
+   readNumber(Peso),
+   write('Altura'), nl,
+   readNumber(Altura),
+   write('Plano de Pagamento[mensal/trimestral/anual]: '), nl,
+   readString(Plano),
+   write('Primeira vez em uma academia?[sim/nao]:'), nl,
+   readString(Resposta),
    addUsersInfo([Id, Nome, Idade, Peso, Altura, Plano, Resposta], Data, Output),
+   write('Cadastro com sucesso!'),
    begin(Output).
 
 
-opcoes(Opcao, Data) :-
-   Opcao =:= 1 -> cadastrarUsuario(Data);
+help :-
+   writeln('--- Como o Appademia pode te ajudar? ---'),
+   writeln('1 - Termos do mundo da musculação.'),
+   writeln('2 - Planos de pagamento.'),
+   writeln('3 - Dicas para novatos.'),
+   writeln('4 - Cancelamento de matricula.'),
+   writeln('5 - Tabela IMC.'),
+   writeln('Opcao escolhida -> ').
+
+
+toStringUser(usuario(Nome, Idade, Peso, Altura, Plano, Resposta, Membership, Treino)):-
+   nl,
+   write('Nome: '),write(Nome),nl,
+   write('Idade: '),write(Idade),nl,
+   write('Peso: '),write(Peso),nl,
+   write('Altura: '),write(Altura),nl,
+   write('Plano: '),write(Plano),nl,
+   write('Iniciante: '),write(Resposta),nl,
+   write('Membro desde: '),write(Membership),nl,
+   write('Treino: '),write(Treino),nl.
+
+options(Opcao, Data) :-
+   Opcao =:= 1 -> signUpUser(Data);
+   Opcao =:= 2 -> updateTraining(Data);
+   Opcao =:= 3 -> updateWeight(Data);
+   Opcao =:= 4 -> showUser(Data);
+   Opcao =:= 5 -> dietInput(Data);
+   Opcao =:= 6 -> help;
    Opcao =:= 7 -> halt(0).
 
 begin(Data) :-
    menu(),
-   input('   Opcao escolhida -> ', Opcao),
-   opcoes(Opcao, Data).
+   write('   Opcao escolhida -> '), nl,
+   readNumber(Opcao),
+   options(Opcao, Data).
 
 menu :-
    nl, writeln('-------------------------------- Bem vindo ao Appademia! --------------------------------
@@ -40,6 +76,7 @@ menu :-
    6. Ajuda 
    7. Sair'), nl.
 
+:- initialization(main).
 main :-
    usersData(Data),
    begin(Data).     
